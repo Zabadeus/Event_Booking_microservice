a0cafaee3d2ef1e8ba51a67bc501e9659485d83a
Zabadeus <m.b.disy_new-guy@hotmail.de>
2020-08-21 19:06:21 +0200

    Inital MircroService - Event_Booking_App

---
+++ client/src/PostCreate.js
@@ -0,0 +1,33 @@
+import React, { useState} from 'react';
+import axios from 'axios';
+
+export default () => {
+    const [title, setTitle] = useState('');
+
+    const onSubmit = async(event) => {
+        event.preventDefault();
+        
+        if (title){
+            await axios.post('http://posts.com/posts/create', {title});
+
+            setTitle('');
+        } else {
+            alert('Title of post is required.')
+        }
+        
+    };
+
+    return (<div>
+        <form onSubmit={onSubmit}>
+            <div className="form-group">
+                <label>Title </label>
+                <input
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                    className="form-control"
+                />
+            </div>
+            <button className="btn btn-primary">Submit</button>
+        </form>
+    </div>);
+};
